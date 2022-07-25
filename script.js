d1d2aa5965961695a7ad6407076d01d00a848dd1
raikiran07 <raikiran925@gmail.com>
2022-07-25 13:23:11 +0530

    contact form is made functional

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ menuBtn.addEventListener("click",()=>{
     }
 
 })
+
+// const name = document.getElementById("username");
+// const email = document.getElementById("useremail").value;
+
+// function sendEmail(){
+//     Email.send({
+//         secureToke:"6a9e30be-594e-40cb-a9cd-347d7163dd4d",
+//         Host : "smtp.elasticemail.com",
+//         Username : "raikiran925@gmail.com",
+//         Password : "57CAE5FB58C89B0158FA08C09C672327C713",
+//         To : 'raikiran925@gmail.com',
+//         From : document.getElementById("useremail").value,
+//         Subject : document.getElementById("username").value,
+//         Body : document.getElementById("usermessage").value  + email
+//     }).then(
+//       message => alert(message)
+//     );
+// }
